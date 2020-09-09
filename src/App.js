c800326b1d4ac7bce825481cbd50371af9623cdb
iltommi1995 <tomasdanielavilavisintin@yahoo.it>
2020-09-09 21:51:33 +0200

    trasferita roba

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./App.css";
+import Slider from './components/organisms/slider';
+import Header from './components/organisms/header';
+
+export default function App() {
+  return (
+    <div class="container">
+      <Header />
+      <Slider />
+    </div>
+  )
+}
