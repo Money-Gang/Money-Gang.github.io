4d6ba22e7f3ef1727d5f381c6d32783e87a5ce7a
Allan Nava <allannava95@gmail.com>
2020-09-13 22:36:15 +0200

    update automated site

---
+++ precache-manifest.fea73e45ca2e1539f6ee829eb90308c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79e11294d7fa0e4e4fd5a1bf7d09508",
+    "url": "/Money-Gang.github.io/index.html"
+  },
+  {
+    "revision": "199fdb4e74d3c1782a64",
+    "url": "/Money-Gang.github.io/static/css/main.0319f190.chunk.css"
+  },
+  {
+    "revision": "a899f8507cf8591e72d0",
+    "url": "/Money-Gang.github.io/static/js/2.52e697c6.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Money-Gang.github.io/static/js/2.52e697c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199fdb4e74d3c1782a64",
+    "url": "/Money-Gang.github.io/static/js/main.e65c8c9a.chunk.js"
+  },
+  {
+    "revision": "4a3caff353d6163df177",
+    "url": "/Money-Gang.github.io/static/js/runtime-main.3a2b1208.js"
+  }
+]);
