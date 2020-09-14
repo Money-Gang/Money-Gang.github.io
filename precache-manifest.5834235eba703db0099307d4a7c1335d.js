fb53db005d1f8d037c76e7eb87b3ecce5e27cde5
Allan Nava <allannava95@gmail.com>
2020-09-14 08:25:11 +0200

    update automated site

---
+++ precache-manifest.5834235eba703db0099307d4a7c1335d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dc3b2e0a4411b7bcf26271eeb3064c",
+    "url": "/Money-Gang.github.io/index.html"
+  },
+  {
+    "revision": "ae2f144ecf9b6a0dd20e",
+    "url": "/Money-Gang.github.io/static/css/main.0319f190.chunk.css"
+  },
+  {
+    "revision": "a899f8507cf8591e72d0",
+    "url": "/Money-Gang.github.io/static/js/2.52e697c6.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Money-Gang.github.io/static/js/2.52e697c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2f144ecf9b6a0dd20e",
+    "url": "/Money-Gang.github.io/static/js/main.7c20c31d.chunk.js"
+  },
+  {
+    "revision": "4a3caff353d6163df177",
+    "url": "/Money-Gang.github.io/static/js/runtime-main.3a2b1208.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Money-Gang.github.io/precache-manifest.34f13e9c2157cd27204cdaf6c99b28c9.js"
+  "/Money-Gang.github.io/precache-manifest.5834235eba703db0099307d4a7c1335d.js"
 );
 
 self.addEventListener('message', (event) => {
